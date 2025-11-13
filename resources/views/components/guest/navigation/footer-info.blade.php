c173f083342a591c77018fcb1267e23f212873a8
Hugo Girona <hugo.girona@student.hepl.be>
2025-11-13 18:26:42 +0100

    Created base layout components abd installed alpine.js for the burger menu

--- resources/views/components/guest/navigation/footer-info.blade.php
+++ resources/views/components/guest/navigation/footer-info.blade.php
@@ -29,7 +29,7 @@
 @endphp
 
 <li class="flex items-center justify-center gap-3 ">
-    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 flex-shrink-0" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         {!! $iconSvg !!}
     </svg>
 
